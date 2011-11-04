ecf0945eb1abe75162cc40ae2c4c60da41c55a95
Paul Nelson <Pablosan@SalientBlue.com>
2011-11-03 21:48:42 -0700

    Update to fix wget/puppet problems
    Trying adding "sudo -u ubuntu" to the "wget" and "puppet" commands as
    they didn't seem to work.

--- dev_workstation_instance.sh
+++ dev_workstation_instance.sh
@@ -4,8 +4,8 @@
 apt-get --assume-yes install puppet
 
 # Get the correct Puppet Manifest
-wget --no-check-certificate https://github.com/Pablosan/ec2_create_instance/raw/master/manifests/dev_workstation.pp
+sudo -u ubuntu wget --no-check-certificate https://github.com/Pablosan/ec2_create_instance/raw/master/manifests/dev_workstation.pp
 
 # Apply manifest
-puppet apply dev_workstation.pp
+sudo -u ubuntu puppet apply dev_workstation.pp
 
