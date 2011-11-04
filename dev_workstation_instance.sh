084e7d01d9aa0077f297a834b1a74162b2101568
Paul Nelson <Pablosan@SalientBlue.com>
2011-11-03 21:57:14 -0700

    Still trying to get wget to work
    I think the problem is that the -O option was not being specified so I'm
    not sure where the downloaded file was being put (or attempted to
    write). Trying adding -O /home/ubuntu/ to see if that works.

--- dev_workstation_instance.sh
+++ dev_workstation_instance.sh
@@ -4,7 +4,7 @@
 apt-get --assume-yes install puppet
 
 # Get the correct Puppet Manifest
-sudo -u ubuntu wget --no-check-certificate https://github.com/Pablosan/ec2_create_instance/raw/master/manifests/dev_workstation.pp
+sudo -u ubuntu wget --no-check-certificate https://github.com/Pablosan/ec2_create_instance/raw/master/manifests/dev_workstation.pp -O /home/ubuntu/
 
 # Apply manifest
 sudo -u ubuntu puppet apply dev_workstation.pp
