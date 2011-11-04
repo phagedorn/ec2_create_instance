87ec4aadfc6eb0550c07bc839ea1854f5dee4c73
Paul Nelson <Pablosan@SalientBlue.com>
2011-11-04 00:12:47 -0700

    Tweaks to oh-my-zsh install
    Decided to do a git clone rather than try and run the install.sh script.
    I think it will be cleaner, and I will get my .zshrc file from my
    machine_config git repository (once I actually create that repository).

--- manifests/dev_workstation.pp
+++ manifests/dev_workstation.pp
@@ -19,23 +19,8 @@ package { "zsh":
 }
 
 exec { "get-oh-my-zsh":
-  command => "/usr/bin/wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh",
+  command => "/usr/bin/git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh",
   cwd     => "/home/ubuntu",
   user    => "ubuntu",
-  require => Package["zsh"]
-}
-
-exec { "chmod-oh-my-zsh":
-  command => "chmod +x /home/ubuntu/install.sh",
-  cwd     => "/home/ubuntu",
-  user    => "ubuntu",
-  require => Exec["get-oh-my-zsh"]
-}
-
-exec { "run-oh-my-zsh":
-  command => "/home/ubuntu/install.sh",
-  cwd     => "/home/ubuntu",
-  user    => "ubuntu",
-  require => Exec["chmod-oh-my-zsh"]
 }
 
