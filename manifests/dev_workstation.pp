47400ee3d0a3ca6997b95d0ada5df7c8afcd45b6
Paul Nelson <Pablosan@SalientBlue.com>
2011-11-05 00:31:29 -0500

    Continue fleshing out dev_workstation Manifest
    - Added sections for pulling machine-config from git.
    - Changed to single quotes because double quotes are unnecessary.
    - Added a user resource to set ubuntu's shell to zsh.

--- manifests/dev_workstation.pp
+++ manifests/dev_workstation.pp
@@ -1,27 +1,40 @@
-file { "/home/ubuntu/src":
+file { '/home/ubuntu/src':
   ensure => directory,
   owner  => ubuntu,
   group  => ubuntu
 }
 
-file { "/home/ubuntu/tmp":
+file { '/home/ubuntu/tmp':
   ensure => directory,
   owner  => ubuntu,
   group  => ubuntu
 }
 
-package { "git":
+package { 'git':
   ensure => installed
 }
 
-package { "zsh":
+package { 'zsh':
   ensure => installed
 }
 
-exec { "get-oh-my-zsh":
-  command => "/usr/bin/git clone https://github.com/robbyrussell/oh-my-zsh.git /home/ubuntu/.oh-my-zsh",
-  cwd     => "/home/ubuntu",
-  user    => "ubuntu",
-  require => Package["git"]
+exec { 'get-oh-my-zsh':
+  command => '/usr/bin/git clone https://github.com/robbyrussell/oh-my-zsh.git /home/ubuntu/.oh-my-zsh',
+  cwd     => '/home/ubuntu',
+  user    => 'ubuntu',
+  require => Package['git']
+}
+
+exec { 'get-machine-config':
+  command => '/usr/bin/git clone git://github.com/Pablosan/machine-config.git',
+  cwd     => '/home/ubuntu',
+  user    => 'ubuntu',
+  require => Package['git']
+}
+
+user { 'ubuntu':
+  ensure  => present,
+  shell   => '/usr/bin/zsh',
+  require => [Package['zsh'], Exec['get-oh-my-zsh', 'get-machine-config']]
 }
 
