ab522d06d6fca40e24b44394c26fffa252a544a3
Paul Nelson <Pablosan@SalientBlue.com>
2011-11-05 01:23:03 -0500

    Continuing work on Manifest
    Had a few hiccups due to git mandating that cloning be done in an empty
    directory Changing tactics: instead of cloning machine-config straight
    into ubuntu's home directory, I will clone it into
    /home/ubuntu/src/machine-config and then create symlinks. That seems
    cleaner anyway.

--- manifests/dev_workstation.pp
+++ manifests/dev_workstation.pp
@@ -1,13 +1,13 @@
 file { '/home/ubuntu/src':
-  ensure => directory,
-  owner  => ubuntu,
-  group  => ubuntu
+  ensure  => directory,
+  owner   => ubuntu,
+  group   => ubuntu
 }
 
 file { '/home/ubuntu/tmp':
-  ensure => directory,
-  owner  => ubuntu,
-  group  => ubuntu
+  ensure  => directory,
+  owner   => ubuntu,
+  group   => ubuntu
 }
 
 package { 'git':
@@ -21,20 +21,24 @@ package { 'zsh':
 exec { 'get-oh-my-zsh':
   command => '/usr/bin/git clone https://github.com/robbyrussell/oh-my-zsh.git /home/ubuntu/.oh-my-zsh',
   cwd     => '/home/ubuntu',
-  user    => 'ubuntu',
-  require => Package['git']
+  user    => 'ubuntu',  require => Package['git'],  creates => '/home/ubuntu/.oh-my-zsh'
 }
 
 exec { 'get-machine-config':
-  command => '/usr/bin/git clone git://github.com/Pablosan/machine-config.git .',
-  cwd     => '/home/ubuntu',
+  command => '/usr/bin/git clone --no-checkout https://github.com/Pablosan/machine-config.git',
+  cwd     => '/home/ubuntu/src',
   user    => 'ubuntu',
-  require => Package['git']
+  require => [Package['git'], File['/home/ubuntu/src']],
+  creates => ['/home/ubuntu/src/machine-config/.gitconfig',
+              '/home/ubuntu/src/machine-config/.gitignore',
+              '/home/ubuntu/src/machine-config/.vimrc',
+              '/home/ubuntu/src/machine-config/.zshrc',
+              '/home/ubuntu/src/machine-config/.vim']
 }
 
 user { 'ubuntu':
   ensure  => present,
   shell   => '/usr/bin/zsh',
-  require => [Package['zsh'], Exec['get-oh-my-zsh', 'get-machine-config']]
+  require => Package['zsh']
 }
 
