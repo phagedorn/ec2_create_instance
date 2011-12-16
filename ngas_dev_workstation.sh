0be6b5235f94381cc8a45d0f581978d79fb1a834
Paul Nelson <Pablosan@SalientBlue.com>
2011-12-16 15:17:45 +0800

    Fixed problems with mismatched filenames
    This has made it obvious that there is duplication in this script that
    should be removed.

--- ngas_dev_workstation.sh
+++ ngas_dev_workstation.sh
@@ -7,9 +7,9 @@ apt-get --assume-yes install puppet
 wget --no-check-certificate https://github.com/Pablosan/ec2_create_instance/raw/master/manifests/ngas_dev_workstation.pp -O /home/ubuntu/ngas_dev_workstation.pp
 
 # Give ownership to ubuntu
-chown ubuntu /home/ubuntu/dev_workstation.pp
-chgrp ubuntu /home/ubuntu/dev_workstation.pp
+chown ubuntu /home/ubuntu/ngas_dev_workstation.pp
+chgrp ubuntu /home/ubuntu/ngas_dev_workstation.pp
 
 # Apply manifest
-puppet apply /home/ubuntu/ngas/dev_workstation.pp
+puppet apply /home/ubuntu/ngas_dev_workstation.pp
 
