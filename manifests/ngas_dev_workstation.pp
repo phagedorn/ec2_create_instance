3f1ee4ebef82c134d444541310e54c7ec2af6b5b
Paul Nelson <Pablosan@SalientBlue.com>
2011-12-16 15:09:45 +0800

    Added ngas setup scripts

---
+++ manifests/ngas_dev_workstation.pp
@@ -0,0 +1,80 @@
+file { '/home/ubuntu/src':
+  ensure  => directory,
+  owner   => ubuntu,
+  group   => ubuntu
+}
+
+file { '/home/ubuntu/tmp':
+  ensure  => directory,
+  owner   => ubuntu,
+  group   => ubuntu
+}
+
+package {
+  'git':               ensure => installed;
+  'zsh':               ensure => installed;
+  'build-essential':   ensure => installed;
+  'python-dev':        ensure => installed;
+  'python-virtualenv': ensure => installed;
+  'libdb-dev':         ensure => installed;
+  'autoconf':          ensure => installed;
+  'libtool':           ensure => installed;
+  'sendmail':          ensure => installed;
+}
+
+exec { 'get-oh-my-zsh':
+  command => '/usr/bin/git clone https://github.com/robbyrussell/oh-my-zsh.git /home/ubuntu/.oh-my-zsh',
+  cwd     => '/home/ubuntu',
+  user    => 'ubuntu',  require => Package['git'],  creates => '/home/ubuntu/.oh-my-zsh'
+}
+
+exec { 'get-machine-config':
+  command => '/usr/bin/git clone https://github.com/Pablosan/machine-config.git',
+  cwd     => '/home/ubuntu/src',
+  user    => 'ubuntu',
+  require => [Package['git'], File['/home/ubuntu/src']],
+  creates => ['/home/ubuntu/src/machine-config/gitconfig',
+              '/home/ubuntu/src/machine-config/.gitignore',
+              '/home/ubuntu/src/machine-config/vimrc',
+              '/home/ubuntu/src/machine-config/zshrc',
+              '/home/ubuntu/src/machine-config/vim']
+}
+
+user { 'ubuntu':
+  ensure  => present,
+  shell   => '/usr/bin/zsh',
+  require => Package['zsh']
+}
+
+file { '/home/ubuntu/.gitconfig':
+  ensure => symlink,
+  owner => ubuntu,
+  group => ubuntu,
+  target => '/home/ubuntu/src/machine-config/gitconfig',
+  require => Exec['get-machine-config']
+}
+
+file { '/home/ubuntu/.vimrc':
+  ensure => symlink,
+  owner => ubuntu,
+  group => ubuntu,
+  target => '/home/ubuntu/src/machine-config/vimrc',
+  require => Exec['get-machine-config']
+}
+
+file { '/home/ubuntu/.zshrc':
+  ensure => symlink,
+  owner => ubuntu,
+  group => ubuntu,
+  target => '/home/ubuntu/src/machine-config/zshrc',
+  require => Exec['get-machine-config']
+}
+
+file { '/home/ubuntu/.vim':
+  ensure => symlink,
+  owner => ubuntu,
+  group => ubuntu,
+  target => '/home/ubuntu/src/machine-config/vim',
+  require => Exec['get-machine-config']
+}
+
---
+++ ngas_dev_workstation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install puppet
+apt-get --assume-yes install puppet
+
+# Get the correct Puppet Manifest
+wget --no-check-certificate https://github.com/Pablosan/ec2_create_instance/raw/master/manifests/ngas_dev_workstation.pp -O /home/ubuntu/ngas_dev_workstation.pp
+
+# Give ownership to ubuntu
+chown ubuntu /home/ubuntu/dev_workstation.pp
+chgrp ubuntu /home/ubuntu/dev_workstation.pp
+
+# Apply manifest
+puppet apply /home/ubuntu/ngas/dev_workstation.pp
+
